20af68e8cd857f4ab01fb2861a01b2db6c54cee9
Mukesh Rana <mukeshrana90@gmail.com>
2019-06-05 21:28:55 +0530

    Update

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,22 +1,28 @@
 import React, { Component } from 'react'
 import $ from 'jquery'
+import ReactPlayer from 'react-player'
 
 export default class Banner extends Component {
 
     componentDidMount() {
-        $('.moveToBtn').click(function(e) {
+        $('.moveToBtn').click(function (e) {
             e.preventDefault();
-            $('body,html').stop().animate({scrollTop:$($(this).attr('href')).offset().top - 145 + 'px'}, 500);
+            $('body,html').stop().animate({ scrollTop: $($(this).attr('href')).offset().top - 145 + 'px' }, 500);
         })
     }
 
     render() {
         return (
             <section className="videoSection">
-                <iframe src="https://www.youtube.com/embed/9910Rui-JFs?controls=0" frameborder="0" allow="accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture"></iframe>
-
-                {/* <iframe src="https://www.youtube-nocookie.com/embed/9910Rui-JFs?version=3&enablejsapi=1&html5=1&volume=100&hd=1&wmode=opaque&showinfo=0&rel=0;;origin=https://orwell.biz;&controls=0&playsinline=1"></iframe> */}
-
+                <ReactPlayer 
+                    url='https://www.youtube.com/watch?v=9910Rui-JFs' 
+                    playing = 'true'
+                    playsinline = 'true'
+                    volume = '0'
+                    width='100%'
+                    height='100%'
+                />
+                <div className="videoOverlay"></div>
                 <a href="#CosaFacciamo" className="moveToBtn"></a>
             </section>
         )
